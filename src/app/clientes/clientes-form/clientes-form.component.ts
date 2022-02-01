febc285633ed3b0d33fe496b2c9d268b20186026
Thiago Dourado <thiagocfdourado@gmail.com>
2022-02-01 16:47:59 -0300

    Criação dos alertas no formulário

--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -11,6 +11,8 @@ import { ClientesService } from '../../clientes.service'
 export class ClientesFormComponent implements OnInit {
 
   cliente: Cliente;
+  success: boolean = false;
+  errors: String[];
 
   constructor( private service : ClientesService) {
     this.cliente = new Cliente();
@@ -23,7 +25,12 @@ export class ClientesFormComponent implements OnInit {
     this.service
     .salvar(this.cliente)
     .subscribe(response => {
-      console.log(response);
+      this.success = true;
+      this.errors = null;
+      this.cliente = response;
+    }, errorResponse => {
+      this. success = false;
+      this.errors = errorResponse.error.errors;
     })
   }
 
