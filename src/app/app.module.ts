2e1bb66e0b4514bf07f8f5cbbf6615894b285b11
Thiago Dourado <thiagocfdourado@gmail.com>
2022-02-01 16:09:30 -0300

    Comunicação com a api através do service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { ClientesModule } from './clientes/clientes.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,6 +17,7 @@ import { ClientesService } from './clientes.service';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     TemplateModule,
     ClientesModule
--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -13,14 +13,18 @@ export class ClientesFormComponent implements OnInit {
   cliente: Cliente;
 
   constructor( private service : ClientesService) {
-    this.cliente = service.getCliente();
+    this.cliente = new Cliente();
    }
 
   ngOnInit(): void {
   }
 
   onSubmit() {
-    console.log(this.cliente);
+    this.service
+    .salvar(this.cliente)
+    .subscribe(response => {
+      console.log(response);
+    })
   }
 
 }
