a06b02f97d82a21b03962cc3e6eb8317ab4740fe
Thiago Dourado <thiagocfdourado@gmail.com>
2022-02-02 10:26:29 -0300

    Listagem de clientes

--- src/app/clientes.service.ts
+++ src/app/clientes.service.ts
@@ -16,11 +16,9 @@ export class ClientesService {
     return this.http.post<Cliente>('http://localhost:8080/api/clientes', cliente);
   }
 
-  getCliente() : Cliente {
-    let cliente : Cliente = new Cliente();
-    cliente.nome = 'Fulano de tal';
-    cliente.cpf = '888888';
-    return cliente;
-
+  getClientes() : Observable<Cliente[]> {
+    return this.http.get<Cliente[]>('http://localhost:8080/api/clientes');
   }
+
+
 }
---
+++ src/app/clientes/clientes-lista/clientes-lista.component.ts
@@ -0,0 +1,23 @@
+import { ClientesService } from './../../clientes.service';
+import { Cliente } from './../clientes-form/cliente';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-clientes-lista',
+  templateUrl: './clientes-lista.component.html',
+  styleUrls: ['./clientes-lista.component.css']
+})
+export class ClientesListaComponent implements OnInit {
+
+  clientes: Cliente[] = [];
+
+  constructor(private service : ClientesService) { }
+
+  ngOnInit(): void {
+    this.service
+    .getClientes()
+      .subscribe(resposta => this.clientes = resposta);
+    ;
+  }
+
+}
